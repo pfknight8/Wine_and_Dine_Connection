9658d03167f1e1febf75bbde06f6b7c8dbc1f3c5
Patrick Knight <pfknight2@gmail.com>
2022-07-25 10:51:44 -0400

    more setup, add wine model

---
+++ models/wine.js
@@ -0,0 +1,17 @@
+const { Schema } = require('mongoose')
+
+const WineSchema = new Schema(
+  {
+    name: { type: String, required: true},
+    description: { type: String, required: true },
+    varietal: { type: String, required: true },
+    category: { type: String, required: true },
+    country: { type: String, required: true },
+    region: { type: String, required: true },
+    price_range: { type: String, required: true },
+    image: {type: String, required: false }
+  },
+  { timestamps: true }
+)
+
+module.exports = WineSchema
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const db = require('./db')
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(express.json())
+app.use(logger('dev'))
+app.use(cors())
+
+app.use('/', routes) //maybe better to use /wineDB ?
+
+db.on('error', console.error.bind(console, 'MongoDB connection error!'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))

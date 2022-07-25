4e75d5a67e1a2cf3a5694d0ad26399cbf9ca8f5c
Patrick Knight <pfknight2@gmail.com>
2022-07-25 15:18:35 -0400

    1st set of controllers & routes running

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,37 @@
 const { Wine } = require('../models')
 
+// Controllers for the wine page
+
+const getAllWines = async (req, res) => {
+  try {
+    let wines = await Wine.find({})
+    return res.status(200).json({ wines })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 const getWine = async (req, res) => {
+  try {
+    let { id } = req.params
+    let wine = await Wine.find({ _id: id })
+    return res.status(200).json({ wine })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const placeWine = async (req, res) => {
+  try {
+    const wine = await new Wine(req.body)
+    await wine.save()
+    return res.status(200).json({ wine })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updateWine = async (req, res) => {
   try {
     return res.status(200).json({})
   } catch (error) {
@@ -8,6 +39,12 @@ const getWine = async (req, res) => {
   }
 }
 
+// Controllers for the meals page
+
+
 module.exports = {
-  getWine
+  getAllWines,
+  getWine,
+  placeWine,
+  updateWine
 }
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,22 @@ const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
 
+// Routes for the wines page
+router.get('/wines/winelist', controllers.getAllWines)
+
+router.get('/wines/:id', controllers.getWine)
+
+router.post('/wines', controllers.placeWine)
+
+router.put('/wines', controllers.updateWine)
+
+// Routes for the meals page
+// router.get()
+
+// router.post()
+
+// router.put()
+
+// router.delete()
+
 module.exports = router

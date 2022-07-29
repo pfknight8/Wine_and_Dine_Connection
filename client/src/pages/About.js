e42eab2be585c8336b8862d7cdbabecf5f094759
Patrick Knight <pfknight2@gmail.com>
2022-07-29 12:17:15 -0400

    Editing some text

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,12 @@ const About = () => {
   // Render (return)
   return (
     <div className="About">
-      <p>Something will go here</p>
+      <h1>About This Site</h1>
+      <p>This site was created by me, Patrick Knight, as part of an immersive bootcamp in software engineering. It constitutes my second project, a full-stack web application with full CRUD capabilities.<br />This project was built over the course of a week. Phase 1 was strictly planning, spanning Friday evening (7/22/22) to Monday morning (7/25/22). Coding then began and proceeded until the bitter end, 1:30pm on the following Friday (7/29/22). I intend to edit this project into a more impressive portfolio piece after the course, if time will allow.<br />Why wine you ask? My family has been in the beverage industry for some time, so I grew up around craft beers and fine wine. My wife, Tara, having a passion for fine foods, it made sense to pick this as a topic, bringing my familial background into my work. While not as extensive as it could be (due to the constraints of time), it was a fun project to build, and a great way to build my skills as a programmer.</p>
+      <h3>Thanks for stopping by!</h3>
+      <h2>Credits</h2>
+      <p>For images: imgur, pexels, pixabay</p>
+      <p>For wine info: totalwines, vivino</p>
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,11 +24,17 @@ const Home = (props) => {
   //Render (return)
   return (
     <div className="homePage">
-      <p>Welcome to the Wine and Dine Connection! Here you can browse a selection of wines and meals.</p>
-      <div id="searchBtns">
-        <button id="searchWine" onClick={handleNavClick}>Browse Wines</button>
+      <section id="homeIntro">
+        <h1>Welcome to the Wine & Dine Connection!</h1>
+        <p>Here you can browse a selection of meals and generate appropriate wine pairings.<br />Click on "Browse Meals" to search from our database of meals.</p>
         <button id="searchMeal" onClick={handleNavClick}>Browse Meals</button>
-      </div>
+      </section>
+      <img id='homeImg' src='https://images.pexels.com/photos/8922189/pexels-photo-8922189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="meal with wine"/>
+      <section id="toWineBrowse">
+        <h2>The Wine Section</h2>
+        <p>If you would prefer to view our databse of wines, click on "Browse Wines" instead.<br />If there is a wine missing that you feel deserves to be included, please feel free to add it to the database appropriately.</p>
+        <button id="searchWine" onClick={handleNavClick}>Browse Wines</button>
+      </section>
     </div>
   )
   //Will display the landing page by default, then navigate to components from there.

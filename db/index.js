67b34c8594fcbc3764fd1e8dcd38f735828b187c
Patrick Knight <pfknight2@gmail.com>
2022-07-28 20:07:23 -0400

    setup heroku

--- db/index.js
+++ db/index.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI = "mongodb://127.0.0.1:27017/wineAndDineDatabase";
+// let MONGODB_URI = "mongodb://127.0.0.1:27017/wineAndDineDatabase";
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : "mongodb://127.0.0.1:27017/wineAndDineDatabase";
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  // .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl)
   .then(() => {
     console.log("Successfully connected to MongoDB");
   })
   .catch((e) => {
     console.error("Connection Error", e.message);
   });
-
+mongoose.set('debug', true)
 const db = mongoose.connection;
 
 module.exports = db;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require('morgan')
 const cors = require('cors')
 const db = require('./db')
 const routes = require('./routes')
+const { index } = require('./models/meal')
 
 const PORT = process.env.PORT || 3001
 
@@ -10,9 +11,14 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/wineDB', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error!'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
